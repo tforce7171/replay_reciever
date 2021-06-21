8cf2c95b7aa0eca3721cb58f89bc0ceb4bf158a9
tforce7171 <68318576+tforce7171@users.noreply.github.com>
2021-06-21 15:18:08 +0900

    Add files via upload

--- reciever.rb
+++ reciever.rb
@@ -26,7 +26,7 @@ def UpdateReplayData(file,event)
     end
   end
   @conn.exec("
-    INSERT INTO replay_data (file_name, time_unix, file_url, channel_id, visibility, title, output_channel_id, playlist)
+    INSERT INTO replay_data (filename, time_unix, file_url, channel_id, visibility, title, output_channel_id, playlist)
     VALUES ('#{file.filename}', #{Time.now.to_i},'#{file.url}',#{channel_id},'#{visibility}','#{title}',#{output_channel_id},'#{playlist}')
   ")
 end
